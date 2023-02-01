d7831aad1be66af8903f3159908efd1fd4918073
Mahliyo <budur1202@gmail.com>
2023-02-01 19:27:50 +0300

    let const var

---
+++ Lessons/Lesson-1/script.js
@@ -0,0 +1,40 @@
+// alert("Hello world !");
+// alert("Hello new world !");
+
+let fname = "Mahliyo";
+
+let lastName = "Allayarova";
+
+let age = "21";
+
+let data = `${fname} ${lastName} ${age}yoshda`;
+
+console.log(data);
+
+let kvadrat = true;
+let aylana = false;
+
+console.log(aylana);
+
+let uchburchak;
+
+console.log(uchburchak);
+
+let tortburchak = null;
+
+console.log(tortburchak);
+
+let id = Symbol("id")
+
+console.log(id);
+
+let max = 254546588445665455125552n;
+
+console.log(max);
+
+const yosh = 21;
+
+console.log(yosh);
+
+
+
