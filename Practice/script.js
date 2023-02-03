d94172bfd4b2486af34a42adfb3bb8194e0cde59
Mahliyo <budur1202@gmail.com>
2023-02-03 16:44:52 +0300

    practice -1

---
+++ Practice/script.js
@@ -0,0 +1,34 @@
+
+
+let fname = "Mahliyo";
+
+let lastName = "Allayarova";
+
+let age = "21";
+
+let family = "turmushga chiqmagan"
+
+let davlat = "Uzbekistan"
+
+
+
+let data = `${fname} ${lastName} ${age}yoshda ${family} ${davlat}da yashaydi`;
+
+console.log(data);
+
+
+let english;
+let russian;
+let uzbek;
+
+console.log(english);
+console.log(russian);
+console.log(uzbek);
+
+
+const myAge = 21;
+
+const yourAge = 25;
+
+console.log(myAge);
+console.log(yourAge);
