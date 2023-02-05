8b1a5af76fa99bffca72bf641039a9e464a3c6b4
Mahliyo <budur1202@gmail.com>
2023-02-05 11:19:31 +0300

    commit 1

---
+++ Tasks/task-1/script.js
@@ -0,0 +1,14 @@
+let son = prompt("enter number");
+
+if (son % 2 === 0) {
+  console.log("kiritilgan son juft");
+} else {
+  console.log("kiritilgan son toq");
+}
+
+let ogirlik = 60;
+let boy = 1.62;
+
+let a = ogirlik / boy ** 2;
+
+console.log(a);
