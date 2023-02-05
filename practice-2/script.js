592f6688206d5d11de7b2cf07601ab8570b8ad23
Mahliyo <budur1202@gmail.com>
2023-02-06 00:52:42 +0300

    11 exircise have finished

--- practice-2/script.js
+++ practice-2/script.js
@@ -34,19 +34,17 @@
 
 // let p = 3.14;
 
-
 // let uzunligi = p*d;
 
 // alert(uzunligi);
 
-//masala -5 
+//masala -5
 
 // let a = Number(prompt("kubning a tomonini kiriting"));
 
 // let hajmi = a**3;
 
 // let tolaSirti = 6*(a**2);
-
 
 // alert(`Kubning hajmi ${hajmi}`);
 // alert(`Kubning tola sirti ${tolaSirti}`);
@@ -63,19 +61,93 @@
 
 // let TolaSirti = 2*(a*b+b*c+a*c);
 
-
 // alert(`paralelepipedning hajmi ${hajmi}`);
 // alert(`paralelepipedning tola sirti ${TolaSirti}`);
 
+//masala 7
+
+// let R = Number(prompt("doiraning radiusini kiriting"));
+
+// let p = 3.14;
+
+// let uzunlik = 2 * p * R;
+
+// let yuza = p * R ** 2;
+
+// alert(`Doiraning uzunligi ${uzunlik}`);
+// alert(`Doiraning yuzasi ${yuza}`);
+
+//masala -8
+
+// let a = Number(prompt("a sonini kiriting"));
+
+// let b = Number(prompt("b sonini kiriting"));
+
+// let ortaArif = (a+b)/2;
+
+// alert(`sonlarning o'rta arifmetigi ${ortaArif}ga teng`);
+
+//masala -9
+
+// let a = Number(prompt(" manfiy bo'lmagan a sonini kiriting"));
+
+// let b = Number(prompt(" manfiy bo'lmagan b sonini kiriting"));
+
+// let ortaGeom = (a * b) **(0.5);
+
+// alert(`sonlarning o'rta geometrigi ${ortaGeom}ga teng`);
+
+//masala 10
+
+// let a = Number(prompt(" manfiy bo'lmagan a sonini kiriting"));
+
+// let b = Number(prompt(" manfiy bo'lmagan b sonini kiriting"));
+
+// let sum = a + b;
+// let multip = a * b;
+
+// let kvadratA = a ** 2;
+// let kvadratB = b ** 2;
+
+// alert(`sonlarning yig'indisi ${sum}ga teng `);
+
+// alert(`sonlarning ko'paytmasi ${multip}ga teng `);
+
+// alert(` a sonining kvadrati ${kvadratA}ga teng `);
+
+// alert(` b sonining kvadrati ${kvadratB}ga teng `);
+
+//masala -11
+
+// let a = Number(prompt(" no'lga teng bo'lmagan  a sonini kiriting"));
+
+// let b = Number(prompt(" no'lga teng bo'lmagan b sonini kiriting"));
+
+// let sum = a + b;
+// let multip = a * b;
+
+// alert(`sonlarning yig'indisi ${sum}ga teng `);
+
+// alert(`sonlarning ko'paytmasi ${multip}ga teng `);
+
+// if (a < 0) {
+//   alert(-a);
+// } else if (a > 0) {
+//   alert(a);
+// }
+
+// if (b < 0) {
+//   alert(-b);
+// } else if (b > 0) {
+//   alert(b);
+// }
 
 
-//keyingi 30 talik masala 
+//keyingi 30 talik masala
 
 //masala-1
 
 // let L = Number(prompt("uzunlikni kiriting(sm)"));
-
-
 
 // alert((L-L%100)/100);
 
@@ -87,53 +159,8 @@
 
 //masala-3
 
-let bayt = Number(prompt("baytni kiriting"));
+// let bayt = Number(prompt("baytni kiriting"));
 
-alert((bayt-bayt%1024)/1024);
+// alert((bayt-bayt%1024)/1024);
 
 //masala -4
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
