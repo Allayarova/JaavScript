ad496db65b59f731e5b12b1c67617ae3e40494b6
Mahliyo <budur1202@gmail.com>
2023-02-07 23:59:46 +0300

    second ex

--- Tasks/task-2/script.js
+++ Tasks/task-2/script.js
@@ -1,7 +1,22 @@
+
+//masala -1
+
+// let son = Number(prompt("Butun son kiriting"));
+
+// if (son > 0) {
+//   alert(++son);
+// } else{
+//     alert(son);
+// }
+
+
+//masala -2
+
 let son = Number(prompt("Butun son kiriting"));
 
 if (son > 0) {
   alert(++son);
-} else{
-    alert(son);
+} else {
+  alert(son-2);
 }
+
