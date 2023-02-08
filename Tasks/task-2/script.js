17c02a8090e1189ad9e703dc26ef9e4c0ba26407
Mahliyo <budur1202@gmail.com>
2023-02-08 10:27:18 +0300

    6 ex has finished

--- Tasks/task-2/script.js
+++ Tasks/task-2/script.js
@@ -20,12 +20,32 @@
 
 //masala -3
 
-let son = Number(prompt("Butun son kiriting"));
+// let son = Number(prompt("Butun son kiriting"));
 
-if (son > 0) {
-  alert(++son);
-} else if (son < 0) {
-  alert(son - 2);
-} else if (son == 0) {
-  alert(son = 10);
+// if (son > 0) {
+//   alert(++son);
+// } else if (son < 0) {
+//   alert(son - 2);
+// } else if (son == 0) {
+//   alert(son = 10);
+// }
+
+//masala -4
+
+// let son1 = Number(prompt("Birinchi butun sonni kiriting"));
+
+// let son2 = Number(prompt("Ikkinchi butun sonni kiriting"));
+
+// let son3 = Number(prompt("Uchinchi butun sonni kiriting"));
+
+//masala -6
+
+let a = Number(prompt("Birinchi butun sonni kiriting"));
+
+let b = Number(prompt("Ikkinchi butun sonni kiriting"));
+
+if (a > b) {
+  alert(a);
+} else if (b > a) {
+  alert(b);
 }
