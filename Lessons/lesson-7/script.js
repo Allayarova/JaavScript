416c546252be54906256b48bf5b475d2092727e8
Mahliyo <budur1202@gmail.com>
2023-02-13 19:21:29 +0300

    for operatorlari

--- Lessons/lesson-7/script.js
+++ Lessons/lesson-7/script.js
@@ -1,42 +1,39 @@
 "use strict";
 
-let arr = [
-  [12, 20, ["git", "github", "gitlab"], 30],
-  ["js", "go", "java", "c#"],
-  ["html", "css", "sass"],
-];
+// let arr = [
+//   [12, 20, ["git", "github", "gitlab"], 30],
+//   ["js", "go", "java", "c#"],
+//   ["html", "css", "sass"],
+// ];
 
-console.log(arr[0][2][1]);
+// console.log(arr[0][2][1]);
 
-const user1 = {
-  fName: "Mahliyo",
-  login: "Mahliyo",
-  parol: "120212",
-  email: "email@gmail.com",
-  phoneNumber: "97 867 00 07",
-  year: "2001",
-  now: "2023",
+// const user1 = {
+//   fName: "Mahliyo",
+//   login: "Mahliyo",
+//   parol: "120212",
+//   email: "email@gmail.com",
+//   phoneNumber: "97 867 00 07",
+//   year: "2001",
+//   now: "2023",
 
-  location: {
-    davlat: "UZB",
-    tuman: "Toshkent",
-  },
+//   location: {
+//     davlat: "UZB",
+//     tuman: "Toshkent",
+//   },
 
-  age:function () {
-    let a = this.now - this.year;
-    return a;
+//   age:function () {
+//     let a = this.now - this.year;
+//     return a;
 
-  
-    
+//   }
 
-    
-  }
+// };
 
+//   console.log(user1.age());
 
-  
-};
+let arr = ["ism", "yosh", "yashash manzil"];
 
-  console.log(user1.age());
-  
-
-
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
