34e0e108c9036aeec919302453ff680211cd9c87
Mahliyo <budur1202@gmail.com>
2023-02-15 10:12:32 +0300

    a va b sonlar orasidagi but sonlar

---
+++ Tasks/task-3/script.js
@@ -0,0 +1,11 @@
+//masala 2
+
+let a = Number(prompt(" a butun sonini kirirting  a<b "));
+
+let b = Number(prompt(" b butun sonini kirirting  a<b "));
+
+let sum = "";
+
+for (let i = a; i <= b; i++) {
+  console.log(i);
+}
