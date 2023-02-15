4f6488b2ab7d67a32303d57bbb57f9c6b4c21c7e
Mahliyo <budur1202@gmail.com>
2023-02-15 10:36:34 +0300

    a va b sonlar orasidagi but sonlar kamayish tartibida

--- Tasks/task-3/script.js
+++ Tasks/task-3/script.js
@@ -1,11 +1,22 @@
 //masala 2
 
+// let a = Number(prompt(" a butun sonini kirirting  a<b "));
+
+// let b = Number(prompt(" b butun sonini kirirting  a<b "));
+
+// let sum = "";
+
+// for (let i = a; i <= b; i++) {
+//   console.log(i);
+// }
+
+//masala 3
+
 let a = Number(prompt(" a butun sonini kirirting  a<b "));
 
 let b = Number(prompt(" b butun sonini kirirting  a<b "));
 
-let sum = "";
-
-for (let i = a; i <= b; i++) {
+for (let i = b; i > a; i--) {
   console.log(i);
+
 }
