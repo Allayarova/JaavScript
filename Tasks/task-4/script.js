c2b826562d93e9fd97c56db647899191f8adabb5
Mahliyo <budur1202@gmail.com>
2023-02-17 16:47:36 +0300

    fuction 2 masala

--- Tasks/task-4/script.js
+++ Tasks/task-4/script.js
@@ -1,20 +1,38 @@
+// let a = Number(prompt(" a sonini kiriting "));
+
+// let b = Number(prompt(" b sonini kiriting "));
+
+// let c = Number(prompt(" c sonini kiriting "));
+
+// let d = Number(prompt(" d sonini kiriting "));
+
+// let e = Number(prompt(" e sonini kiriting "));
+
+// function Power(a, b, c, d, e) {
+//   a3 = a ** 3;
+//   b3 = b ** 3;
+//   c3 = c ** 3;
+//   d3 = d ** 3;
+//   e3 = e ** 3;
+//   console.log(a3, b3, c3, d3, e3);
+//   return a3, b3, c3, d3, e3;
+// }
+// Power(a, b, c, d, e);
+
+//masala 2
+
 let a = Number(prompt(" a sonini kiriting "));
 
 let b = Number(prompt(" b sonini kiriting "));
 
 let c = Number(prompt(" c sonini kiriting "));
 
-let d = Number(prompt(" d sonini kiriting "));
-
-let e = Number(prompt(" e sonini kiriting "));
-
-function Power(a, b, c, d, e) {
-  a3 = a ** 3;
+function Power234(a, b, c) {
+  a2 = a ** 2;
   b3 = b ** 3;
-  c3 = c ** 3;
-  d3 = d ** 3;
-  e3 = e ** 3;
-  console.log(a3, b3, c3, d3, e3);
-  return a3, b3, c3, d3, e3;
+  c4 = c ** 4;
+
+  console.log(a2, b3, c4);
+  return a2, b3, c4;
 }
-Power(a, b, c, d, e);
+Power234(a, b, c);
