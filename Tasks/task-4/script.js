9f2416965e7e5728f04769c7fac53b4d730f2316
Mahliyo <budur1202@gmail.com>
2023-02-17 17:02:37 +0300

    fuction 3 masala

--- Tasks/task-4/script.js
+++ Tasks/task-4/script.js
@@ -21,18 +21,43 @@
 
 //masala 2
 
+// let a = Number(prompt(" a sonini kiriting "));
+
+// let b = Number(prompt(" b sonini kiriting "));
+
+// let c = Number(prompt(" c sonini kiriting "));
+
+// function Power234(a, b, c) {
+//   a2 = a ** 2;
+//   b3 = b ** 3;
+//   c4 = c ** 4;
+
+//   console.log(a2, b3, c4);
+//   return a2, b3, c4;
+// }
+// Power234(a, b, c);
+
+//masala 3
+
 let a = Number(prompt(" a sonini kiriting "));
 
 let b = Number(prompt(" b sonini kiriting "));
 
 let c = Number(prompt(" c sonini kiriting "));
 
-function Power234(a, b, c) {
-  a2 = a ** 2;
-  b3 = b ** 3;
-  c4 = c ** 4;
+let d = Number(prompt(" d sonini kiriting "));
 
-  console.log(a2, b3, c4);
-  return a2, b3, c4;
+function MEAN(a, b, c, d) {
+  ortaarif1 = (a + b) / 2;
+  ortaarif2 = (a + c) / 2;
+  ortaarif3 = (a + d) / 2;
+
+  ortageom1 = (a * b) ** 0.5;
+  ortageom2 = (a * c) ** 0.5;
+  ortageom3 = (a * d) ** 0.5;
+
+  console.log(ortaarif1, ortaarif2, ortaarif3);
+  console.log(ortageom1, ortageom2, ortageom3);
+  return ortaarif1, ortaarif2, ortaarif3, ortageom1, ortageom2, ortageom3;
 }
-Power234(a, b, c);
+MEAN(a, b, c, d);
