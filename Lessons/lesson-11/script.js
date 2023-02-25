fdad5128efd6c3405c8b983367fda2131c726381
Mahliyo <budur1202@gmail.com>
2023-02-25 23:46:21 +0300

    set

---
+++ Lessons/lesson-11/script.js
@@ -0,0 +1,37 @@
+"use script";
+
+const user = {
+  fName: "Mahliyo",
+  lName: "Allayarova",
+  age: 22,
+  auth: {
+    login: "Mahliyo",
+    pass: " password",
+    email: "name@gmail.com",
+  },
+};
+
+// console.log(user.auth?.pass);
+// console.log(Object.keys(user));
+
+// console.log(Object.values(user));
+
+// console.log(Object.entries(user));
+
+// for (let el of Object.keys(user)) {
+//   console.log(el);
+// }
+
+// for (let el of Object.values(user)) {
+//   console.log(el);
+// }
+
+// for (let el of Object.entries(user)) {
+//   console.log(`${el[0]}:${el[1]}`);
+// }
+
+//sets
+
+const arr = [12, 13, 14, 15, 16, 17, 18];
+const saveSet = new Set(arr);
+console.log(saveSet);
