6e62dbaceb147cd38e3f7ce238e2786c1e7b1fbc
Mahliyo <budur1202@gmail.com>
2023-03-02 19:52:06 +0300

    splice method

--- Lessons/lesson-14/script.js
+++ Lessons/lesson-14/script.js
@@ -4,9 +4,15 @@ const arr = [12, 13, 14, 15, 1, 7, 18];
 
 //slice (immutable)
 
-let newArr = arr.slice(0, 4);
+// let newArr = arr.slice(0, 4);
+// console.log(newArr);
+
+// console.log(arr);
+
+//splice (mutable)
+
+let newArr = arr.splice(0, 4);
 console.log(newArr);
 
 console.log(arr);
 
-//splice (mutable)
