2abf291cadee05e5d1d22fc0f7c6e76ded6407d0
Mahliyo <budur1202@gmail.com>
2023-03-02 16:29:03 +0300

    slice method

--- Lessons/lesson-13/script.js
+++ Lessons/lesson-13/script.js
@@ -1,3 +1,55 @@
+"use script";
+
+// functionName(12, 13);
+
+//decloration function
+
+// function functionName(a, b) {
+//   let sum = a + b;
+//   console.log(sum);
+
+//   return sum;
+// }
+
+//expression function
+
+// const functionName2 = function (a, b) {
+//   let sum = a - b;
+//   console.log(sum);
+
+//   return sum;
+// };
+
+let arr = [1, 2, 3, 45, 6, 7, 8];
+
+let min = arr[0];
+
+for (let i = 0; i < arr.length; i++) {
+  if (min > arr[i]) {
+    min = arr[i];
+  }
+}
+console.log(min);
+
+// let fuc = function () {};
+
+//arrow function
+
+// const arrowFunction = (a = 1, b = 1) => a * b;
+
+// console.log(arrowFunction(100));
+// console.log(arrowFunction(100, 0.1));
+
+//
+let age = 25;
+const user2 = {
+  age: 30,
+};
+
+const func = (age) => {
+  age = 10;
+};
+
 const user = {
   fName: "Umar",
   lNmae: "Hamidov",
@@ -12,7 +64,7 @@ const user = {
   },
 };
 
-console.log(user.countBirth(21));
+// console.log(user.countBirth(21));
 
 const admin = {
   fName: "Umid",
@@ -26,10 +78,14 @@ const admin = {
 let a = [40, 100];
 let b = 50;
 
-console.log(user.countBirth.call(admin, b));
+// console.log(user.countBirth.call(admin, b));
 
-console.log(user.countBirth.call(admin, ...a));
+// console.log(user.countBirth.call(admin, ...a));
 
 //apply
 
-console.log(user.countBirth.apply(admin, [b]));
+// console.log(user.countBirth.apply(admin, [b]));
+
+// (function () {
+//   alert = "hello";
+// })();
---
+++ Lessons/lesson-14/script.js
@@ -0,0 +1,12 @@
+"use script";
+
+const arr = [12, 13, 14, 15, 1, 7, 18];
+
+//slice (immutable)
+
+let newArr = arr.slice(0, 4);
+console.log(newArr);
+
+console.log(arr);
+
+//splice (mutable)
