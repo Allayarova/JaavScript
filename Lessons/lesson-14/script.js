e045a7bf95000aab4fdaad38b4b834aaa4e0c822
Mahliyo <budur1202@gmail.com>
2023-03-02 19:55:40 +0300

    concat method

--- Lessons/lesson-14/script.js
+++ Lessons/lesson-14/script.js
@@ -2,6 +2,8 @@
 
 const arr = [12, 13, 14, 15, 1, 7, 18];
 
+const arr2 = [1, 3, 4, 5, 7, 8];
+
 //slice (immutable)
 
 // let newArr = arr.slice(0, 4);
@@ -19,10 +21,19 @@ const arr = [12, 13, 14, 15, 1, 7, 18];
 //reverse (mutable)
 
 
-let newArr = arr.reverse(0, 4);
+// let newArr = arr.reverse(0, 4);
+// console.log(newArr);
+
+// console.log(arr);
+
+//concat (immutable )
+
+let newArr = arr.concat(arr2);
 console.log(newArr);
 
 console.log(arr);
+
+
 
 
 
