dae14dcbef956fa8c5fccfc0fcf0d39a751a6746
Allayarova Mahliyo <117286548+Allayarova@users.noreply.github.com>
2023-03-26 12:03:38 +0300

    prepend

---
+++ SelectingElements/script.js
@@ -0,0 +1,21 @@
+"use script";
+
+const parentBox = document.getElementById("parentBox");
+
+const parentBoxFirstChild = document.getElementsByClassName(
+  "parentBox__firstChild"
+);
+
+const changeBtn = document.getElementById("changeBtn");
+
+// const createChild = document.createElement("div");
+
+const changeBtnClicked = () => {
+  const createChild = document.createElement("div");
+  createChild.innerHTML = "<p>Hello world</p>";
+  parentBox.prepend(createChild);    
+};
+
+changeBtn.addEventListener("click", changeBtnClicked);
+
+
